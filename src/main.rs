bcd09eb73ac53031fcc1ad3cc3604436236a3182
Abeo Anthony, A <anthonyabeo@gmail.com>
2019-04-23 00:14:48 +0000

    load game ROM into architectured memory

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use std::env;
-use std::process;
-
 use emulator_intel8080::intel8080::Intel8080;
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: {} - Game ROM file not provided", &args[0]);
-        process::exit(1);
-    }
+    // let args: Vec<String> = env::args().collect();
+    // if args.len() < 2 {
+    //     println!("Usage: {} - Game ROM files not provided", &args[0]);
+    //     process::exit(1);
+    // }
 
-    let machine = Intel8080::new();
+    let mut machine = Intel8080::new();
+    
+    // load invaders.h into memory range 0x0000 - 0x07ff
+    // load invaders.g into memory range 0x0800 - 0x0fff
+    // load invaders.f into memory range 0x1000 - 0x17ff
+    // load invaders.e into memory range 0x1800 - 0x1fff
+    machine.load_game_rom("/Users/anabila/Downloads/invaders/invaders"); 
+
+    // println!("{:?}", &machine.memory[0x0000..=0x17ff]);
     machine.emulate();
 }
