6214e8d1443f138fe372c6e23005d4954f821b0b
Abeo Anthony, A <anthonyabeo@gmail.com>
2019-04-24 08:37:15 +0000

    tests for opcode 3

--- src/tests.rs
+++ src/tests.rs
@@ -14,7 +14,26 @@ fn emulate_opcodes_1_and_2 () {
     ];
 
     machine.emulate();
+
     assert_eq!(machine.regs.b, 0);
     assert_eq!(machine.regs.c, 10);
     assert_eq!(machine.memory[10], 255);
+}
+
+#[test]
+fn emulate_opcodes_3() {
+    let mut machine = Intel8080::new();
+    machine.memory = vec![
+        1, // LXI B
+        0xff, 0x38, 
+        3, // INX B
+        0,
+        0x76, // HLT
+        0, 0, 0, 0, 0
+    ];
+
+    machine.emulate();
+
+    assert_eq!(machine.regs.b, 0x39);
+    assert_eq!(machine.regs.c, 0x00);
 }
