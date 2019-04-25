1b58585974e6b8761234dab844175b4477c6dcb9
Abeo Anthony, A <anthonyabeo@gmail.com>
2019-04-25 09:49:52 +0000

    test for opcodes MVI and RLC added

--- src/tests.rs
+++ src/tests.rs
@@ -39,7 +39,7 @@ fn emulate_opcodes_3() {
 }
 
 #[test]
-fn emulate_opcode_4() {
+fn emulate_opcodes_4_5_6() {
     let mut machine = Intel8080::new();
     machine.regs.b = 0x99;
     machine.memory = vec![
@@ -54,6 +54,7 @@ fn emulate_opcode_4() {
     assert_eq!(machine.flags.sign, 0);
     assert_eq!(machine.flags.parity, 1);
 
+    // opcode 5
     let mut machine = Intel8080::new();
     machine.regs.b = 0x08;
     machine.memory = vec![
@@ -67,4 +68,31 @@ fn emulate_opcode_4() {
     assert_eq!(machine.flags.zero, 0);
     assert_eq!(machine.flags.sign, 0);
     assert_eq!(machine.flags.parity, 0);
+
+    // opcode 6
+    let mut machine = Intel8080::new();
+    machine.memory = vec![
+        6, // opcode
+        45, // data
+        0x76
+    ];
+
+    machine.emulate();
+
+    assert_eq!(machine.regs.b, 45);
+}
+
+#[test]
+fn emulate_opcode_7() {
+    let mut machine = Intel8080::new();
+    machine.regs.a = 0xf2;
+    machine.memory = vec![
+        7, // opcode
+        0x76
+    ];
+
+    machine.emulate();
+
+    assert_eq!(machine.regs.a, 0xe5);
+    assert_eq!(machine.flags.carry, 1);
 }
