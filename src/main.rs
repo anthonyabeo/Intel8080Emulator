7a6af91613a411233aead1abe0f1a9b8a786700b
Abeo Anthony, A <anthonyabeo@gmail.com>
2019-12-31 20:48:10 +0000

    graphix window showing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+extern crate sfml;
+
 use std::env;
 use std::process;
 
 use emulator_intel8080::cpu::intel8080::Intel8080;
+
+use sfml::graphics::*;
+use sfml::window::*;
 
 
 fn main() {
@@ -11,9 +16,25 @@ fn main() {
         process::exit(1);
     }
 
-    let mut machine = Intel8080::new();
-    
-    machine.load_program(&args[1]); 
+    let mut window = RenderWindow::new((800, 600), "SPACE INVADERS", Style::CLOSE, &Default::default());
+    window.set_vertical_sync_enabled(true);
 
+    while window.is_open() {
+        while let Some(event) = window.poll_event() {
+            match event {
+                Event::Closed => {
+                    window.close();
+                    return;
+                }
+                _ => {
+                    window.clear(&Color::BLACK);
+                    window.display();
+                }
+            }
+        }
+    }
+
+    let mut machine = Intel8080::new();
+    machine.load_program(&args[1]); 
     machine.run();
 }
