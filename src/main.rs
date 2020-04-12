b2bc8f58e818d94f55e0d05b5cabbb031886a0ae
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-04-12 21:31:06 +0000

    ready to rock 'n roll

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-extern crate sfml;
+extern crate sdl2; 
 
 use std::env;
 use std::process;
 
 use emulator_intel8080::cpu::intel8080::Intel8080;
 use emulator_intel8080::space_invaders::SpaceInvadersMachine;
-
-// use sfml::graphics::*;
-// use sfml::window::*;
 
 
 fn main() {
@@ -17,43 +14,8 @@ fn main() {
         process::exit(1);
     }
 
-    let machine = Intel8080::new();
-    let mut space_invaders = SpaceInvadersMachine::new(machine);
-    space_invaders.init(&args[1]);
-    space_invaders.start_emulation();
+    let mut state = Intel8080::new();
+    state.load_program(&args[1]);
 
-    // let mut window = RenderWindow::new((1024, 768), "SPACE INVADERS", Style::DEFAULT, &Default::default());
-    // window.set_vertical_sync_enabled(true);
-
-    // while window.is_open() {
-    //     while let Some(event) = window.poll_event() {
-    //         match event {
-    //             Event::Closed => {window.close(); return; },
-    //             Event::KeyPressed { code, .. } => {
-    //                 match code {
-    //                     Key::Space => println!("space key was pressed"),
-    //                     Key::Left => println!("Left arrow key was pressed"),
-    //                     Key::Right => println!("Right arrow key was pressed"),
-    //                     _ => {}
-    //                 }
-    //             },
-    //             Event::KeyReleased { code, .. } => {
-    //                 match code {
-    //                     Key::Space => println!("space key was released"),
-    //                     Key::Left => println!("Left arrow key was released"),
-    //                     Key::Right => println!("Right arrow key was released"),
-    //                     _ => {}
-    //                 }
-    //             }
-    //             _ => {
-    //                 window.clear(&Color::BLACK);
-    //                 window.display();
-    //             }
-    //         }
-    //     }
-    // }
-
-    // let mut machine = Intel8080::new();
-    // machine.load_program(&args[1]); 
-    // machine.run();
+    let _space_invaders = SpaceInvadersMachine::new();
 }
